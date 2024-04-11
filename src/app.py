31c4555f7141c118eb2f1ef069f5c8a004bebe13
ndkma <nicowork13@gmail.com>
2024-04-11 14:42:54 +0800

    feat: modify app_mention handler to ask questions and record answers

--- src/app.py
+++ src/app.py
@@ -5,6 +5,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack import WebClient
 from dotenv import load_dotenv
 
+# Utilities
+from utils.user_bank import existing_users
+from utils.questions import QUESTIONS, answers
+
 # Credentials
 load_dotenv("../.env")
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
@@ -20,16 +24,43 @@ logger = logging.getLogger(__name__)
 
 @app.event(("member_joined_channel"))
 def handle_member_joined_channel(event: dict, say: slack_bolt.Say, logger: logging.Logger) -> None:
-    channel_id = event['channel']
     user_id = event['user']
     bot_id = "U06RWGEU2LX"
-    say(f"Welcome <@{user_id}> to the <#{channel_id}> channel! Please mention me by typing <@{bot_id}> and fill out the subsequent form!")
+    channel_id = event['channel']
+
+    if user_id not in existing_users:
+        say(f"Welcome <@{user_id}> to the <#{channel_id}> channel! Please mention me by typing <@{bot_id}> and fill out the subsequent form!")
+
 
 @app.event(("app_mention"))
 def handle_app_mention_event(body: dict, say: slack_bolt.Say, logger: logging.Logger) -> None:
-    ...
+    user_id = body['event']['user']
+    bot_id = "U06RWGEU2LX"
+    try:
+        channel_id = body["event"]["thread_ts"]
+    except KeyError:
+        channel_id = body["event"]["ts"]
+    thread_timestamp = body["event"]["ts"]
+    
+    try:
+        counter = 1 if user_id not in answers.keys() else len(answers[user_id]) + 1
+        say(QUESTIONS[counter], thread_ts = thread_timestamp)
+        if user_id not in answers.keys():
+            answers[user_id] = [body['event']['text']]
+        else: answers[user_id].append(body['event']['text'])
+        print(answers)
+    except KeyError:
+        say("Thank you for answering my questions. You may close this thread", thread_ts = thread_timestamp)
 
 
+    
+
+
+    # if user_id not in existing_users:
+    #     say(QUESTIONS, thread_ts = thread_timestamp)
+    #     existing_users.append(user_id)
+    # else:
+    #     say("You are not a new user.", thread_ts = thread_timestamp)
 
 
 
---
+++ src/utils/questions.py
@@ -0,0 +1,11 @@
+QUESTIONS = {
+    1: "What's your name, and if you were a superhero, what would your superpower be?",
+    2: "If you could have any fictional character as your best friend, who would it be and why?",
+    3: "What's the most unusual or quirky hobby you have?",
+    4: "If you could instantly become an expert in one thing, what would it be?",
+    5: "What's the funniest or most embarrassing thing that has happened to you at work?"
+} 
+
+answers = {
+    
+}
